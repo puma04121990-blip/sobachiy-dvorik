6527891c19ca7410b03a3b73eae41b434ee88e7d
puma04121990-blip <puma04121990@gmail.com>
2026-09-06 09:51:26 +1000

    Restore game loader after accidental overwrite

--- js/game.js
+++ js/game.js
@@ -1,1 +1,13 @@
-see-file
+(function () {
+  try {
+    var b64 = (window.__GPARTS || []).join('');
+    if (!b64) { console.error('[game] missing packs'); return; }
+    var bin = atob(b64);
+    var bytes = new Uint8Array(bin.length);
+    for (var i = 0; i < bin.length; i++) bytes[i] = bin.charCodeAt(i);
+    var src = new TextDecoder('utf-8').decode(bytes);
+    (0, eval)(src);
+  } catch (e) {
+    console.error('[game pack]', e);
+  }
+})();
